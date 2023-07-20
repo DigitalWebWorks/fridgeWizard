f075f2d2e68298eccce6d7c57677125002cab6ee
Michael Weckop <michaelweckop@michaels-air-2.lan>
2023-07-20 15:32:23 -0400

    Graph and fridge content render simultaneously

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -12,55 +12,62 @@ import Expired from './Expired'
 //import types object from json object in db
 
 export const Dashboard = ({ isLoggedIn, setIsLoggedIn }) => {
-    const [fridgeContents, setFridgeContents] = useState([]);
-    const [isLoading, setIsLoading] = useState(true)
-    const [ESClicked, setESClicked] = useState(false)
-    const [EClicked, setEClicked] = useState(false)
-    const [graphClicked, setgraphClicked] = useState(false)
-    const email = localStorage.getItem('email');
+  const [fridgeContents, setFridgeContents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+  const [ESClicked, setESClicked] = useState(false)
+  const [EClicked, setEClicked] = useState(false)
+  const [graphClicked, setgraphClicked] = useState(false)
+  const email = localStorage.getItem('email');
 
-    useEffect(() => {
-      const getFoodContent = async () => {
-        const res = await getFood(email)
-        setFridgeContents(res);
-        setIsLoading(false);
-      }
-        getFoodContent();
-    }, [])
+  useEffect(() => {
+    const getFoodContent = async () => {
+      const res = await getFood(email)
+      setFridgeContents(res);
+      setIsLoading(false);
+    }
+    getFoodContent();
+  }, [])
 
-    return (
-        <div className='flex flex-col'>
-          <div className='pb-20'>
-            <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-          </div>
-            <div className='flex items-center justify-center'>
-              <div className="w-3/5 md:w-1/2 flex flex-col">
-                <div className='flex items-center justify-between'>
-                  <div className='font-mynerve text-2xl font-semibold'>
-                    Add an item
-                  </div>
-                  <IconButtons 
-                    EClicked={EClicked} 
-                    setEClicked={setEClicked} 
-                    ESClicked={ESClicked} 
-                    setESClicked={setESClicked} 
-                    graphClicked={graphClicked} 
-                    setgraphClicked={setgraphClicked} 
-                    fridgeContents={fridgeContents} 
-                  />
-                </div>
-                <div className='flex flex-col w-3/5 gap-4'>
-                  {ESClicked && <ExpiringSoon fridgeContents={fridgeContents}/>}
-                  {EClicked && <Expired fridgeContents={fridgeContents}/>}
-                </div>
-                <div className='flex flex-col'>
-                  <InputFields email={email} setFridgeContents={setFridgeContents} />
-                  { graphClicked ? <CompositionGraph fridgeContents={fridgeContents}/> :
-                    isLoading ? <LoadingSpinner /> : <Contents email={email} isLoading={isLoading} setFridgeContents={setFridgeContents} fridgeContents={fridgeContents}/>
-                  }
-                </div>
-              </div>
+  return (
+    <div className='flex flex-col'>
+      <div className='pb-20'>
+        <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      </div>
+      <div className='flex items-center justify-center'>
+        <div className="w-3/5 md:w-1/2 flex flex-col">
+          <div className='flex items-center justify-between'>
+            <div className='font-mynerve text-2xl font-semibold'>
+              Add an item
             </div>
+            <IconButtons
+              EClicked={EClicked}
+              setEClicked={setEClicked}
+              ESClicked={ESClicked}
+              setESClicked={setESClicked}
+              graphClicked={graphClicked}
+              setgraphClicked={setgraphClicked}
+              fridgeContents={fridgeContents}
+            />
+          </div>
+          <div className='flex flex-col w-3/5 gap-4'>
+            {ESClicked && <ExpiringSoon fridgeContents={fridgeContents} />}
+            {EClicked && <Expired fridgeContents={fridgeContents} />}
+          </div>
+          <div className='flex flex-col'>
+            <InputFields email={email} setFridgeContents={setFridgeContents} />
+            {/* { graphClicked ? <CompositionGraph fridgeContents={fridgeContents}/>
+                   : isLoading ? <LoadingSpinner /> : <Contents email={email} isLoading={isLoading} setFridgeContents={setFridgeContents} fridgeContents={fridgeContents}/>
+                  } */}
+              {graphClicked &&
+              <>
+                <CompositionGraph fridgeContents={fridgeContents} />
+              </>
+              }
+              {isLoading && <LoadingSpinner />}
+            <Contents email={email} isLoading={isLoading} setFridgeContents={setFridgeContents} fridgeContents={fridgeContents} />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
