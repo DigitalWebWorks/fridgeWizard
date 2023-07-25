413aad3bbbb38a02d170ee97fcca0fe166b85a9e
Bryan <luna.bryanr@gmail.com>
2023-07-25 16:49:08 -0400

    bcrypt implemented for createUser

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,64 +1,64 @@
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcryptjs");
-// const asyncHandler = require("express-async-handler");
-
-// require in user model
-// // for mongoDB:
-// const UserData = require('../models/userModel')
-// for SQL:
+// require in userModelSQL
 const db = require('../models/userModelSQL')
+// require in bcrypt
+const bcrypt = require('bcryptjs');
+
+// define userController object
+const userController = {};
 
 // @description Register new user
 // @route POST /api/users/register
 // @access Public
-const userController = {};
-userController.createUser =  async (req, res, next) => {
-  console.log('entered createUser');
+userController.createUser = async (req, res, next) => {
+  // deconstruct request body
+  const { name, email, password } = req.body;
+  
+  // check for all user inputs; if all inputs not present
+  if (!name || !email || !password) {
+    // return 400 status and throw error
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  // hash the password using Bcrypt
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
   try {
-    // deconstruct request body
-    const { name, email, password } = req.body;
-  
-    // check for all user inputs
-    if (!name || !email || !password) {
-      res.status(400);
-      throw new Error("Please add all fields");
+    // create values array for SQL queries
+    const values = [name, email, hashedPassword];
+    console.log(hashedPassword);
+    // define query to see if user already exists in database
+    const existsUserQuery = 'SELECT email FROM users WHERE email = ($1)';
+    // execute existsUserQuery
+    const userExists = await db.query(existsUserQuery, [email]);
+    console.log('userExists:', userExists)
+
+    // if rows property of userExists has length
+    if (userExists.rows.length) {
+      console.log('userExists data: ', userExists);
+      // user already exists in db - return relevant feedback message
+      return next('User already exists');
     }
-  
-    // check if user exists
-    // for mongoDB:
-    // const userExists = await UserData.findOne({ email });
 
-    // const selectHomeworlds = 'SELECT people.*, planets.name AS homeworld FROM "public"."people" INNER JOIN "public"."planets" ON planets._id = people.homeworld_id';
-    // const selectFilms = 'SELECT people._id, films.title FROM "public"."people" INNER JOIN "public"."people_in_films" ON people_in_films.person_id = people._id INNER JOIN "public"."films" ON films._id = people_in_films.film_id WHERE people._id = $1';
-
-    // for SQL:
-    // const existsQuery = `SELECT email FROM users WHERE email = ${email}`
-
-    // const userExists = await db.query(existsQuery);
-
-    // console.log('userExists', userExists);
-  
-    // if (userExists) {
-    //   return next('User already exists')
-    // }
-
-    const values = [name, email, password];
+    // if userExists is null (i.e. user does not exist in database)
+    // define query to create new user in the database
     const createUserQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`;
+    // execute createUserQuery
     const newUser = db.query(createUserQuery, values);
-  
-    // // for MongoDB:
-    // const newUser = await UserData.create({ username, password, email })
 
     res.locals.newUser = newUser;
   
+    // return invocation of next
     return next();
+  // catch - if error experienced during database queries
   } catch (err) {
-    console.log(err)
+    // log err
+    console.log(err);
+    // return invocation of next, passing in err
     return next(err);
   }
- 
 };
-
 
 // @description Authenticate user data
 // @route POST /api/users/login
@@ -88,41 +88,6 @@ userController.verifyUser = async (req, res, next) => {
   } catch (err) {
     return next(err);
   }
-
-  // check for email and password
-
-//   // find user by email in db
-//   const user = await User.findOne({ email });
-
-// //   // check if user exists and password is correct
-//   if (user && (await bcrypt.compare(password, user.password))) {
-//     res.json({
-//       _id: user.id,
-//       name: user.name,
-//       email: user.email,
-//       token: generateToken(user._id),
-//     });
-//     return next()
-//   } else {
-//     res.status(400);
-//     throw new Error("Invalid Credentials");
-//   }
 };
-
-
-// @description send to home page
-// @route GET /api/users/home
-// @access Private
-userController.goHome = async (req, res) => {
-//   res.redirect('/home')
-};
-
-// Generate token
-// const generateToken = (id) => {
-//   // will sign a new token with the id passed in with the secret used and will expire in 30 days
-//   return jwt.sign({ id }, process.env.JWT_SECRET, {
-//     expiresIn: "30d",
-//   });
-// };
 
 module.exports = userController;
